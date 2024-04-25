f3348f899456e54f9b00592a65ff0189e535552f
Rimantas <belauzas@gmail.com>
2024-04-25 14:48:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// Single line comment
+
+/*
+Multi
+line
+comment
+*/
+
+// CTRL + S = issaugoti faila
+// CTRL + A = pasirinkti visa teksta
+// CTRL + Z = grizti vienu veiksmu atgal
+// CTRL + X = iskirpti teksta
+// CTRL + V = iklijuoti teksta
+
+console.log(11111);
+console.log(222222222222);
+console.log(3333);
+
+/*
+Pagrindines programavimo temos:
+- duomenu tipai
+- palyginimo salygos
+- ciklai
+- funkcijos
+- class (oop)
+- event listeners
+*/
+
+/*
+Duomenu tipai:
+- number (skaiciai)
+- string (tekstas)
+- boolean (logine reiksme)
+- array (masyvas)
+- object (objektas)
+- null
+- undefined
+*/
