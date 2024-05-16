1ad471fb51461b885d2f1b0674afe82f53382d1c
Rimantas <belauzas@gmail.com>
2024-05-16 13:27:11 +0300

    Update

--- js/duomenys/string.js
+++ js/duomenys/string.js
@@ -1,7 +1,7 @@
 /*
 STRING - tekstas, tekstiniu simboliu grandinele
 
-iniciavimas: ' (vienguba), " (dvyguba)
+iniciavimas: ' (vienguba), " (dvyguba), ` (backtick)
 
 */
 
@@ -50,3 +50,84 @@ Pinigines turinys: 10;
 Pinigine papildyta: 90;
 Pinigines turinys: 100;
 */
+
+/*
+<header>
+    <img>
+    <nav>
+        <a></a>
+        <a></a>
+        <a></a>
+    </nav>
+</header>
+*/
+console.clear();
+// \r - return
+// \n - new line
+// \t - tab
+const HTML = '\
+<header>\r\n\
+\t<img>\r\n\
+\t<nav>\r\n\
+\t\t<a></a>\r\n\
+\t\t<a></a>\r\n\
+\t\t<a></a>\r\n\
+\t</nav>\r\n\
+</header>';
+
+console.log(HTML);
+
+const backtick = `Labas rytas, Lietuva!`;
+console.log(backtick);
+
+const backtick1 = `Backtikine (\`) kabute.`;
+const backtick2 = 'Backtikine (`) kabute.';
+const backtick3 = "Backtikine (`) kabute.";
+
+const HTML2 = `<header>
+    <img>
+    <nav>
+        <a></a>
+        <a></a>
+        <a></a>
+    </nav>
+</header>`;
+console.log(HTML2);
+
+console.clear();
+const a = 7;
+const b = 5;
+const c = a + b;
+
+// 7 + 5 = 12
+const ats1 = a + ' + ' + b + ' = ' + c;
+console.log(ats1);
+
+const ats2 = a + " + " + b + " = " + c;
+console.log(ats2);
+
+const ats3 = `${a} + ${b} = ${c}`;
+console.log(ats3);
+
+
+const start = 0;
+const finish = 11;
+const step = 3;
+const answer = 4;
+
+const mini1 = 'Skaičių intervale tarp ' + start + ' ir ' + finish + ', besidalijančių be liekanos iš ' + step + ' yra ' + answer + ' vienetai.';
+
+console.log(mini1);
+
+const mini2 = `Skaičių intervale tarp ${start} ir ${finish}, besidalijančių be liekanos iš ${step} yra ${answer} vienetai.`;
+console.log(mini2);
+
+const gg = 'Good game!';
+const number = 'Number: ' + answer;
+
+console.log(mini1.length);
+console.log(mini1[0]);
+
+
+console.log('' + true);
+console.log(true.toString());
